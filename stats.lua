28a953e347d0d472f6ecb0ad5e1841c08ef4f188
axunaattori <69193142+axunaattori@users.noreply.github.com>
2024-02-07 18:15:13 +0200

    Add stats.lua
    remember to actually make this

---
+++ stats.lua
@@ -0,0 +1,27 @@
+-- made by axunaattori
+ 
+local component = require("component")
+local gpu = component.gpu
+local term = require("term")
+ 
+sp = 2
+lives = 2
+score = 0
+hs = 0
+power = 1.00
+function update()
+  gpu.setBackground(0x00FF00)
+  gpu.setForeground(0xFF00FF)
+  
+  term.setCursor(70, 10)
+  io.write("LIVES: ")
+  for i = 1, lives do
+    io.write("♥")
+    ix = i
+  end
+  gpu.setForeground(0x800080)
+  for i = 1, 8-ix do
+    io.write("♥")
+  end 
+end
+ 
